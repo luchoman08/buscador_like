602d9a83f9583773e60cda442d8747005c17f35a
luchoman <luchoman08@gmail.com>
2016-02-07 14:09:57 -0500

    hpp con la estructura de cada nodo

---
+++ nodo.hpp
@@ -0,0 +1,31 @@
+#ifndef nodo_hpp
+#define nodo_hpp
+
+#include <vector>
+
+public class Nodo{
+
+private:
+char caracter_actual;
+char caracter_siguiente;
+char caracter_anterior;
+int id_cadena;
+ 
+
+public:
+Nodo();
+~Nodo();
+
+char get_caracter_actual();
+char get_caracter_siguiente();
+char get_caracter_anterior();
+int get_id_cadena();
+
+void set_caracter_actual(char nuevo_caracter);
+void set_caracter_siguiente(char nuevo_caracter);
+void set_caracter_anterior(char nuevo_caracter);
+void set_id_cadena(int nuevo_id);
+};
+
+
+#endif
